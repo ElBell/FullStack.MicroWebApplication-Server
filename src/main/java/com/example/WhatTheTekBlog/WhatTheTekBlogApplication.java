ba3c819bf00726db97ca0e388d11adfca5dd74e1
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-51.local>
2019-04-24 02:33:04 -0400

    added cors

--- src/main/java/com/example/WhatTheTekBlog/WhatTheTekBlogApplication.java
+++ src/main/java/com/example/WhatTheTekBlog/WhatTheTekBlogApplication.java
@@ -41,7 +41,7 @@ public class WhatTheTekBlogApplication {
 		return new WebMvcConfigurerAdapter() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("https://whatthetek-client.herokuapp.com");
+				registry.addMapping("/**");
 			}
 		};
 	}
